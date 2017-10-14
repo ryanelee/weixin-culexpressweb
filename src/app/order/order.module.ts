ebeeb411bbfed36c05686bd613907558b44d5983
TimLiu1 <995332120@qq.com>
2017-10-14 17:59:33 +0800

    bug fix

--- src/app/order/order.module.ts
+++ src/app/order/order.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { OrderRoutingModule } from './order-routing.module';
+import { OrderListComponent } from './order-list/order-list.component';
 
 @NgModule({
   imports: [
     CommonModule,
     OrderRoutingModule
   ],
-  declarations: []
+  declarations: [OrderListComponent]
 })
 export class OrderModule { }
