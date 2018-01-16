fe8018ea338a57955fd7b069309bf30206306894
Tim Z Liu <tim.z.liu@pwc.com>
2018-01-16 20:49:17 +0800

    bug fix

--- src/app/routes/login/login.component.ts
+++ src/app/routes/login/login.component.ts
@@ -39,18 +39,26 @@ export class LoginComponent implements OnInit {
         console.log('result', result);
         console.log('email', result.emailAddress);
         const wxuser: WxUser = this._auth.getWxUser();
-        this._user.updateWxUserInfo({ email: result.emailAddress, openid: wxuser.openid }).subscribe((update_result: any) => {
-          if (update_result.err) {
-            alert('err' + update_result.err);
-          } else {
-            const data: User = result;
-            this._auth.setUser(result);
-            this._auth.setJwtToken(result.token);
-            // this._user
-            this._router.navigate(['user-profile']);
-          }
-          ;
-        })
+        if (wxuser) {
+          this._user.updateWxUserInfo({ email: result.emailAddress, openid: wxuser.openid }).subscribe((update_result: any) => {
+            if (update_result.err) {
+              alert('err' + update_result.err);
+            } else {
+              const data: User = result;
+              this._auth.setUser(result);
+              this._auth.setJwtToken(result.token);
+              // this._user
+              this._router.navigate(['user-profile']);
+            }
+            ;
+          })
+        } else {
+          this._auth.setUser(result);
+          this._auth.setJwtToken(result.token);
+          // this._user
+          this._router.navigate(['user-profile']);
+        }
+
 
       }
     })
