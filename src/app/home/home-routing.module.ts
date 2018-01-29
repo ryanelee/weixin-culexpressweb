d7a2c38dc6d41592b04281880c5d2e321e582825
En Li <eleeryan@gmail.com>
2018-01-28 20:19:24 -0800

    added channel intro page

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -3,10 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 import { ChannelIntroComponent } from 'app/home/channel-intro/channel-intro.component';
+import { EmbargoComponent } from 'app/home/embargo/embargo.component';
 
 const routes: Routes = [
   {
     path: 'channel-intro', component: ChannelIntroComponent
+  },
+  {
+    path: 'embargo', component: EmbargoComponent
   }
 ];
 
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,19 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatExpansionPanel, MatExpansionModule, MatTab, MatTabsModule } from '@angular/material';
+import { MatGridListModule, MatGridList } from '@angular/material';
 
 import { HomeComponent } from 'app/home/home.component';
 import { ChannelIntroComponent } from 'app/home/channel-intro/channel-intro.component';
 import { HomeRoutingModule } from 'app/home/home-routing.module';
 import { AddValueIntroComponent } from './channel-intro/add-value-intro/add-value-intro.component';
+import { EmbargoComponent } from './embargo/embargo.component';
 
 @NgModule({
   imports: [
     CommonModule,
     MatExpansionModule,
     MatTabsModule,
+    MatGridListModule,
     HomeRoutingModule
   ],
-  declarations: [HomeComponent, ChannelIntroComponent, AddValueIntroComponent]
+  declarations: [HomeComponent, ChannelIntroComponent, AddValueIntroComponent, EmbargoComponent]
 })
 export class HomeModule { }
