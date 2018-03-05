8058b361965fc2d80ab99145ae5bf276c03c017a
En Li <eleeryan@gmail.com>
2018-03-05 15:16:16 -0800

    updated id card uploader

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,11 @@
 
 export const environment = {
   production: false,
-  // api: 'http://localhost:8000/api',
-  api: 'http://api.culexpress.com/api',
-  wechatApi: 'http://localhost:8080'
+  api: 'http://localhost:8000/api',
+  // api: 'http://api.culexpress.com/api',
+  wechatApi: 'http://localhost:8080',
+  wxConfig: {
+    appId: 'wxbea82c7ca7971d97',
+    appSecret: '63514d00948cb51f5cbb0952340ce0c1'
+  }
 };
